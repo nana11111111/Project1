c90bcc544cd28171a920f477f57d9c12dbf331eb
nana11111111 <155862932+nana11111111@users.noreply.github.com>
2024-06-14 00:00:58 +0100

    Add files via upload

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  importOrder: ['^[./]', '^@components/(.*)$'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+
+  // .prettierrc
+
+  plugins: ['prettier-plugin-tailwindcss'],
+};
